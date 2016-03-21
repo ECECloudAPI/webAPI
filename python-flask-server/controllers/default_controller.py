d4e7741811ba9818425e9540e81bae9c58f1312b
skralick <kralicks@gmail.com>
2016-03-21 15:05:29 -0400

    adding prelim server code swagger generated

---
+++ python-flask-server/controllers/default_controller.py
@@ -0,0 +1,84 @@
+
+def buidlings_building_id_robots_delete(buildingId) -> str:
+    return 'do some magic!'
+
+def buidlings_building_id_robots_get(buildingId) -> str:
+    return 'do some magic!'
+
+def buidlings_building_id_robots_post(buildingId) -> str:
+    return 'do some magic!'
+
+def buidlings_building_id_sensors_delete(buildingId) -> str:
+    return 'do some magic!'
+
+def buidlings_building_id_sensors_get(buildingId) -> str:
+    return 'do some magic!'
+
+def buidlings_building_id_sensors_post(buildingId) -> str:
+    return 'do some magic!'
+
+def buildings_building_id_delete(buildingId) -> str:
+    return 'do some magic!'
+
+def buildings_building_id_get(buildingId) -> str:
+    return 'do some magic!'
+
+def buildings_building_id_put(buildingId) -> str:
+    return 'do some magic!'
+
+def buildings_delete() -> str:
+    return 'do some magic!'
+
+def buildings_post() -> str:
+    return 'do some magic!'
+
+def robots_delete() -> str:
+    return 'do some magic!'
+
+def robots_get() -> str:
+    return 'do some magic!'
+
+def robots_robot_id_delete(robotId) -> str:
+    return 'do some magic!'
+
+def robots_robot_id_get(robotId) -> str:
+    return 'do some magic!'
+
+def robots_robot_id_put(robotId) -> str:
+    return 'do some magic!'
+
+def sensors_delete() -> str:
+    return 'do some magic!'
+
+def sensors_get() -> str:
+    return 'do some magic!'
+
+def sensors_post() -> str:
+    return 'do some magic!'
+
+def sensors_sensor_id_delete(sensorId) -> str:
+    return 'do some magic!'
+
+def sensors_sensor_id_get(sensorId) -> str:
+    return 'do some magic!'
+
+def sensors_sensor_id_put(sensorId) -> str:
+    return 'do some magic!'
+
+def users_delete() -> str:
+    return 'do some magic!'
+
+def users_get() -> str:
+    return 'do some magic!'
+
+def users_post() -> str:
+    return 'do some magic!'
+
+def users_user_id_delete(userId) -> str:
+    return 'do some magic!'
+
+def users_user_id_get(userId) -> str:
+    return 'do some magic!'
+
+def users_user_id_put(userId) -> str:
+    return 'do some magic!'
