ddee4eb388e22244d3b46a22b7ec9f504b1b0abc
walkerks <walkerks@vt.edu>
2016-03-22 02:05:57 -0400

    Added more of the models for pyanomdb

--- python-flask-server/Models.py
+++ python-flask-server/Models.py
@@ -33,26 +33,36 @@ class Building(Model):
 	robots = UnicodeSetAttribute()
 
 
-'''
+
 class Room(Model):
-	__metadata__ = {
-		'throughput': {
-			'read': 1,
-			'write': 1,
-		}
-	}
-	roomId = Field(hash_key=True)
-	roomName = Field(data_type=str)
-	statuses = Field(data_type=list)
-	width = Field(data_type=int)
-	height = Field(data_type=int)
-	length = Field(data_type=int)
-	sensors = Field(data_type=list)
-	robots = Field(data_type=list)
+	class Meta:
+        	table_name = 'Room'
+	roomId = UnicodeAttribute(hash_key=True)
+	roomName = UnicodeAttribute()
+	statuses = UnicodeSetAttribute()
+	width = NumberAttribute()
+	height = NumberAttribute()
+	length = NumberAttribute()
+	sensors = UnicodeSetAttribute()
+	robots = UnicodeSetAttribute()
 
 
 
+class Robot(Model):
+	class Meta:
+        	table_name = 'Robot'
+	robotId = UnicodeAttribute(hash_key=True)
+	emergency = BooleanAttribute()
+	offensive = BooleanAttribute()
+	sensors = UnicodeSetAttribute()
+	buildingId = NumberAttribute()
+	name = UnicodeAttribute()
+	movement = UnicodeAttribute()
+	updated = UTCDateTimeAttribute()
+	floor = NumberAttribute()
+	roomId = NumberAttribute()
 
+'''
 class Users(Model):
     userid = Field(hash_key=True)
     location = Field(range_key=True)
@@ -60,21 +70,10 @@ class Users(Model):
     text = Field()
 
 
-
-
-
-class Robot(Model):
-	robotId = Field(hash_key=True)
-	emergency = Field(data_type=bool)
-	offensive = Field(data_type=bool)
-	sensors = Field(data_type=list)
-	buildingId = Field(data_type=int)
-	name = Field(data_type = str)
-	movement = Field(data_type = str)
-	updated = Field()
-	floor = Field(data_type = int)
-	roomId = Field(data_type = int)
 '''
+
+
+
 
 
 
--- python-flask-server/createTables.py
+++ python-flask-server/createTables.py
@@ -1,5 +1,6 @@
-
-from Models import  Sensor, Building
+from Models import  Sensor, Building, Robot, Room
 
 Sensor.create_table(read_capacity_units=1, write_capacity_units=1)
 Building.create_table(read_capacity_units=1, write_capacity_units=1)
+Robot.create_table(read_capacity_units=1, write_capacity_units=1)
+Room.create_table(read_capacity_units=1, write_capacity_units=1)
