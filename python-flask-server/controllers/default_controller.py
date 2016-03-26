02737318a9a9ce6c85e992ecf829ed7625e02744
skralick <skralick@vt.edu>
2016-03-26 00:05:08 -0400

    Updated api yaml and regenerated code so put requests can put objects and post requests return objects to api callers can get the ids and then modify their respective resources

--- python-flask-server/controllers/default_controller.py
+++ python-flask-server/controllers/default_controller.py
@@ -17,22 +17,13 @@ def buidlings_building_id_sensors_get(buildingID) -> str:
 def buidlings_building_id_sensors_post(buildingID) -> str:
     return 'do some magic!'
 
-def buildings_building_id_delete(buildingID) -> str:
-    return 'do some magic!'
-
-def buildings_building_id_get(buildingID) -> str:
-    return 'do some magic!'
-
-def buildings_building_id_put(buildingID) -> str:
-    return 'do some magic!'
-
 def robots_robot_id_delete(robotID) -> str:
     return 'do some magic!'
 
 def robots_robot_id_get(robotID) -> str:
     return 'do some magic!'
 
-def robots_robot_id_put(robotID) -> str:
+def robots_robot_id_put(robotID, newRobot) -> str:
     return 'do some magic!'
 
 def sensors_sensor_id_delete(sensorID) -> str:
@@ -41,7 +32,7 @@ def sensors_sensor_id_delete(sensorID) -> str:
 def sensors_sensor_id_get(sensorID) -> str:
     return 'do some magic!'
 
-def sensors_sensor_id_put(sensorID) -> str:
+def sensors_sensor_id_put(sensorID, newSensor) -> str:
     return 'do some magic!'
 
 def users_user_id_delete(userID) -> str:
@@ -50,13 +41,7 @@ def users_user_id_delete(userID) -> str:
 def users_user_id_get(userID) -> str:
     return 'do some magic!'
 
-def users_user_id_put(userID) -> str:
-    return 'do some magic!'
-
-def buildings_delete() -> str:
-    return 'do some magic!'
-
-def buildings_post() -> str:
+def users_user_id_put(userID, newUser) -> str:
     return 'do some magic!'
 
 def robots_delete() -> str:
