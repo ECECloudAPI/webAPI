f825441da7965027fd6d599e8430ff4a5c03c334
1nv0ke <changsun@vt.edu>
2016-03-28 15:25:01 -0400

    Fixed some bugs.

--- test_api.sh
+++ test_api.sh
@@ -1,21 +1,6 @@
 
-# POST buildings/
-curl -X POST -H "Content-Type: application/json" -H "Cache-Control: no-cache" -H "Postman-Token: 50b14706-3ec7-770c-806d-1d96ddc94a81" -d '{
-    "id" : "topSecret",
-    "buildingId" : "6",
-    "building" : "LaveryHall",
-    "rooms" : ["a", "b"],
-    "owner" : 2,
-    "users" : ["Chang", "Apple"],
-    "sensors" : ["m", "n"],
-    "robots" : ["R2"]
-}' "http://0.0.0.0:8080/api/buildings/"
-
 # POST sensors/
-curl -X POST -H "Content-Type: application/json" -H "Cache-Control: no-cache" -H "Postman-Token: a74e3fc2-9f63-a014-6c71-aa98fb2536d4" -d '{
-    "sensorId" : "3",
-    "sensorType" : "Top Secret",
-    "buildingId" : "6",
-    "roomId" : "404",
-    "data" : ["foo", "bar"]
-}' "http://0.0.0.0:8080/api/sensors/"
+curl -X POST -H "Cache-Control: no-cache" -H "Postman-Token: 02bd02a2-6e5d-daaf-ef8e-ff2d353dd46a" -d '' "http://0.0.0.0:8080/api/sensors/"
+
+# GET sensors/{sensorID}
+curl -X GET -H "Cache-Control: no-cache" -H "Postman-Token: c61f9cdc-c509-4b05-d05b-7dbdee88d107" "http://0.0.0.0:8080/api/sensors/ca519d63-e81a-4df1-bdb1-ffe85df435ce"
