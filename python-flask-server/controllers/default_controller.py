ca241410d4685738aaee9d54fca7f564beff5887
alexsupp <alexsupp@users.noreply.github.com>
2016-04-05 13:35:37 -0400

    Update default_controller.py
    Updates to the variable names to match the new version 1.0.4 standards

--- python-flask-server/controllers/default_controller.py
+++ python-flask-server/controllers/default_controller.py
@@ -4,156 +4,156 @@ import uuid
 from flask import request, jsonify
 from storageModels import Building, Sensor, User, Robot
 
-def buildings_building_id_robots_delete(buildingID) -> str:
+def buildings_building_id_robots_delete(buildingId) -> str:
     resultList = []
-    for item in Robot.scan(buildingID__eq=buildingID):
+    for item in Robot.scan(buildingId__eq=buildingId):
         resultList.append(item.delete())
     return "%s items deleted." % len(resultList)
 
-def buildings_building_id_robots_get(buildingID) -> str:
+def buildings_building_id_robots_get(buildingId) -> str:
     resultList = []
-    for item in Robot.scan(buildingID__eq=buildingID):
+    for item in Robot.scan(buildingId__eq=buildingId):
         resultList.append(item.attribute_values)
     return ( resultList )
 
-def buildings_building_id_robots_post(buildingID) -> str:
+def buildings_building_id_robots_post(buildingId) -> str:
     try:
-        building = Building.get(buildingID)
+        building = Building.get(buildingId)
     except Exception as e:
         print(e)
-        return 'Building with id %s does not exist.' % (buildingID)
+        return 'Building with id %s does not exist.' % (buildingId)
     newID = str(uuid.uuid4())
-    newObj = Robot(id=newID, buildingID=buildingID)
+    newObj = Robot(id=newID, buildingId=buildingId)
     newObj.save()
     return newObj.attribute_values
 
-def buildings_building_id_sensors_delete(buildingID) -> str:
+def buildings_building_id_sensors_delete(buildingId) -> str:
     resultList = []
-    for item in Sensor.scan(buildingID__eq=buildingID):
+    for item in Sensor.scan(buildingId__eq=buildingId):
         resultList.append(item.delete())
     return "%s items deleted." % len(resultList)
 
-def buildings_building_id_sensors_get(buildingID) -> str:
+def buildings_building_id_sensors_get(buildingId) -> str:
     resultList = []
-    for item in Sensor.scan(buildingID__eq=buildingID):
+    for item in Sensor.scan(buildingId__eq=buildingId):
         resultList.append(item.attribute_values)
     return ( resultList )
 
-def buildings_building_id_sensors_post(buildingID) -> str:
+def buildings_building_id_sensors_post(buildingId) -> str:
     try:
-        building = Building.get(buildingID)
+        building = Building.get(buildingId)
     except Exception as e:
         print(e)
-        return 'Building with id=%s does not exist.' % (buildingID)
+        return 'Building with id=%s does not exist.' % (buildingId)
     newID = str(uuid.uuid4())
-    newObj = Sensor(id=newID, buildingID=buildingID)
+    newObj = Sensor(id=newID, buildingId=buildingId)
     newObj.save()
     return newObj.attribute_values
 
-def buildings_building_id_delete(buildingID) -> str:
+def buildings_building_id_delete(buildingId) -> str:
     try:
-        building = Building.get(buildingID)
+        building = Building.get(buildingId)
         building.delete()
     except Exception as e:
-        return 'Building with id=%s does not exist.' % (buildingID)
-    return 'Successfully deleted buildingID=%s.' % (buildingID)
+        return 'Building with id=%s does not exist.' % (buildingId)
+    return 'Successfully deleted buildingId=%s.' % (buildingId)
 
-def buildings_building_id_get(buildingID) -> str:
+def buildings_building_id_get(buildingId) -> str:
     try:
-        building = Building.get(buildingID)
+        building = Building.get(buildingId)
     except Exception as e:
         print(e)
-        return 'Building with id=%s does not exist.' % (buildingID)
+        return 'Building with id=%s does not exist.' % (buildingId)
     return building.attribute_values
 
-def buildings_building_id_put(buildingID, newBuilding) -> str:
+def buildings_building_id_put(buildingId, newBuilding) -> str:
     try:
-        building = Building.get(buildingID)
+        building = Building.get(buildingId)
     except Exception as e:
-        return 'Building with id=%s does not exist.' % (buildingID)
+        return 'Building with id=%s does not exist.' % (buildingId)
     attributes = building.attribute_values.keys()
     for key in newBuilding.keys():
         if key in attributes and key is not 'id':
             building.update_item(key, value=newBuilding[key], action='PUT')
-    return 'Building with id=%s updated successfully.' % (buildingID)
+    return 'Building with id=%s updated successfully.' % (buildingId)
 
-def robots_robot_id_delete(robotID) -> str:
+def robots_robot_id_delete(robotId) -> str:
     try:
-        robot = Robot.get(robotID)
+        robot = Robot.get(robotId)
         robot.delete()
     except Exception as e:      
-        return 'Robot with id=%s does not exist.' % (robotID)
-    return 'Successfully deleted robot with id=%s.' % (robotID)
+        return 'Robot with id=%s does not exist.' % (robotId)
+    return 'Successfully deleted robot with id=%s.' % (robotId)
 
-def robots_robot_id_get(robotID) -> str:
+def robots_robot_id_get(robotId) -> str:
     try:
-        robot = Robot.get(robotID)
+        robot = Robot.get(robotId)
     except Exception as e:
-        return 'Robot with id=%s does not exist.' % (robotID)
+        return 'Robot with id=%s does not exist.' % (robotId)
     return robot.attribute_values
 
-def robots_robot_id_put(robotID, newRobot) -> str:
+def robots_robot_id_put(robotId, newRobot) -> str:
     try:
-        robot = Robot.get(robotID)
+        robot = Robot.get(robotId)
     except Exception as e:
-        return 'Robot with id=%s does not exist.' % (robotID)
+        return 'Robot with id=%s does not exist.' % (robotId)
     attributes = robot.attribute_values.keys()
     for key in newRobot.keys():
         if key in attributes and key is not 'id':
             robot.update_item(key, value=newRobot[key], action='PUT')
-    return 'Robot with id=%s updated successfully.' % (robotID)
+    return 'Robot with id=%s updated successfully.' % (robotId)
 
-def sensors_sensor_id_delete(sensorID) -> str:
+def sensors_sensor_id_delete(sensorId) -> str:
     try:
-        sensor = Sensor.get(sensorID)
+        sensor = Sensor.get(sensorId)
         sensor.delete()
     except Exception as e:
-        return 'Sensor with id=%s does not exist.' % (sensorID)
-    return 'Successfully deleted sensor with id=%s.' % (sensorID)
+        return 'Sensor with id=%s does not exist.' % (sensorId)
+    return 'Successfully deleted sensor with id=%s.' % (sensorId)
 
-def sensors_sensor_id_get(sensorID) -> str:
+def sensors_sensor_id_get(sensorId) -> str:
     try:
-        sensor = Sensor.get(sensorID)
+        sensor = Sensor.get(sensorId)
     except Exception as e:
-        return 'Sensor with id=%s does not exist.' % (sensorID)
+        return 'Sensor with id=%s does not exist.' % (sensorId)
     return sensor.attribute_values
 
-def sensors_sensor_id_put(sensorID, newSensor) -> str:
+def sensors_sensor_id_put(sensorId, newSensor) -> str:
     try:
-        sensor = Sensor.get(sensorID)
+        sensor = Sensor.get(sensorId)
     except Exception as e:
-        return 'Sensor with id=%s does not exist.' % (sensorID)
+        return 'Sensor with id=%s does not exist.' % (sensorId)
     attributes = sensor.attribute_values.keys()
     for key in newSensor.keys():
         if key in attributes and key is not 'id':
             sensor.update_item(key, value=newSensor[key], action='PUT')
-    return 'Sensor with id=%s updated successfully.' % (sensorID)
+    return 'Sensor with id=%s updated successfully.' % (sensorId)
 
-def users_user_id_delete(userID) -> str:
+def users_user_id_delete(userId) -> str:
     try:
-        user = User.get(userID)
+        user = User.get(userId)
         user.delete()
     except Exception as e:      
-        return 'User with id=%s does not exist.' % (userID)
-    return 'Successfully deleted user with id=%s.' % (userID)
+        return 'User with id=%s does not exist.' % (userId)
+    return 'Successfully deleted user with id=%s.' % (userId)
 
-def users_user_id_get(userID) -> str:
+def users_user_id_get(userId) -> str:
     try:
-        user = User.get(userID)
+        user = User.get(userId)
     except Exception as e:
-        return 'User with id=%s does not exist.' % (userID)
+        return 'User with id=%s does not exist.' % (userId)
     return user.attribute_values
 
-def users_user_id_put(userID, newUser) -> str:
+def users_user_id_put(userId, newUser) -> str:
     try:
-        user = User.get(userID)
+        user = User.get(userId)
     except Exception as e:
-        return 'User with id=%s does not exist.' % (userID)
+        return 'User with id=%s does not exist.' % (userId)
     attributes = user.attribute_values.keys()
     for key in newUser.keys():
         if key in attributes and key is not 'id':
             user.update_item(key, value=newUser[key], action='PUT')
-    return 'User with id=%s updated successfully.' % (userID)
+    return 'User with id=%s updated successfully.' % (userId)
 
 def buildings_delete() -> str:
     temp = []
